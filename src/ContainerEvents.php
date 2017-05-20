d3999002527850a4873be6bcc7b6b03226a48032
litepubl <yarrowsoft@gmail.com>
2017-05-21 01:05:21 +0300

    rc7.08 events updated

--- src/ContainerEvents.php
+++ src/ContainerEvents.php
@@ -3,17 +3,22 @@
 namespace litepubl\core\options;
 
 use litepubl\core\container\EventsInterface as ContainerEventsInterface;
+use litepubl\core\container\EventsInterface as ContainerInterface;
 use litepubl\core\storage\StorageAware;
+use litepubl\core\events\EventManagerInterface as ContainerEventsInterface;
 
 class ContainerEvents implements ContainerEventsInterface, StorageAware
 {
     const RESULT = 'result';
     const INSTANCE = 'instance';
     const CLASSNAME = 'className';
+
+    protected $container;
     protected $eventManager;
 
-    public function __construct(EventsInterface $eventManager)
+    public function __construct(ContainerInterface $container, EventManagerInterface $eventManager)
     {
+        $this->container = $container;
         $this->eventManager = $eventManager;
     }
 
@@ -30,34 +35,34 @@ class ContainerEvents implements ContainerEventsInterface, StorageAware
 
     public function onBeforeGet(string $className)
     {
-        $a = $this->eventManager->call('onBeforeGet', [static::CLASSNAME => $className, static::RESULT => null]);
+        $a = $this->eventManager->trigger('onBeforeGet', $this->container, [static::CLASSNAME => $className, static::RESULT => null]);
         return a[static::RESULT];
     }
 
     public function onAfterGet(string $className, $instance)
     {
-        $this->eventManager->call('onAfterGet', [static::CLASSNAME => $className, static::INSTANCE => $instance]);
+        $this->eventManager->trigger('onAfterGet', $this->container, [static::CLASSNAME => $className, static::INSTANCE => $instance]);
     }
 
     public function onSet($instance, string $name)
     {
-        $this->eventManager->call('onSet', ['name' => $name, static::INSTANCE => $instance]);
+        $this->eventManager->trigger('onSet', $this->container, ['name' => $name, static::INSTANCE => $instance]);
     }
 
     public function onBeforeCreate(string $className)
     {
-        $a = $this->eventManager->call('onBeforeGet', [static::CLASSNAME => $className, static::RESULT => null]);
+        $a = $this->eventManager->trigger('onBeforeGet', $this->container, [static::CLASSNAME => $className, static::RESULT => null]);
         return a[static::RESULT];
     }
 
     public function onAfterCreate(string $className, $instance)
     {
-        $this->eventManager->call('onAfterCreate', [static::CLASSNAME => $className, static::INSTANCE => $instance]);
+        $this->eventManager->trigger('onAfterCreate', $this->container, [static::CLASSNAME => $className, static::INSTANCE => $instance]);
     }
 
     public function onNotFound(string $className)
     {
-        $a = $this->eventManager->call('onBeforeGet', [static::CLASSNAME => $className, static::RESULT => null]);
+        $a = $this->eventManager->trigger('onBeforeGet', $this->container, [static::CLASSNAME => $className, static::RESULT => null]);
         return a[static::RESULT];
     }
 }
